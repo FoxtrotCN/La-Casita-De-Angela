d6e8100f495a9af80bff8ae99143ee4af825fe23
Foxtrot <fernandoestarlin121000@gmail.com>
2022-07-04 12:26:00 -0400

    The images respective to any product is now visible.

--- Shop/urls.py
+++ Shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 
 urlpatterns = [
@@ -10,4 +12,4 @@ urlpatterns = [
     path('make/', views.make_products, name="make"),
     path('edit/', views.edit_products, name="edit")
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
