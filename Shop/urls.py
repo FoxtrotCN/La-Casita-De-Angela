a80f1cac0e2c930f43415e72de1271b9cd9e72d1
Foxtrot <fernandoestarlin121000@gmail.com>
2022-07-04 16:34:01 -0400

    The user now can log in to the main page

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import static
 
 
+
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
     path('shop/', views.shop, name="shop"),
     path('products/', views.products, name="products"),
     path('main/', views.main, name="main"),
