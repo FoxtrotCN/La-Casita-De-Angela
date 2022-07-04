93f918921bac4d1bbc818b0caea7fddb08bf9ab6
Foxtrot <fernandoestarlin121000@gmail.com>
2022-07-04 11:59:01 -0400

    The models file was modify. Now we can delete the products and its image.

--- Shop/models.py
+++ Shop/models.py
@@ -6,3 +6,11 @@ class Product(models.Model):
     name = models.CharField("Nombre", max_length=100)
     quantity = models.IntegerField("Cantidad")
     image = models.ImageField("Imagen", upload_to='images/', null=False)
+
+    # def __str__(self):
+    #     row = "Nombre: " + self.name + "-" + "Cantidad: " + str(self.quantity)
+    #     return row
+
+    def delete(self, using=None, keep_parents=False):
+        self.image.storage.delete(self.image.name)
+        super().delete()
