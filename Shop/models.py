949a7d080e27da32746c15a1829bde5be3bd17cd
Foxtrot <fernandoestarlin121000@gmail.com>
2022-07-05 12:44:27 -0400

    Now the code is all in English

--- Shop/models.py
+++ Shop/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField("Nombre", max_length=100)
-    quantity = models.IntegerField("Cantidad")
-    image = models.ImageField("Imagen", upload_to='images/', null=False)
+    name = models.CharField("Name", max_length=100)
+    quantity = models.IntegerField("Quantity")
+    image = models.ImageField("Image", upload_to='images/', null=False)
 
     def __str__(self):
-        row = "Nombre: " + self.name + "-" + "Cantidad: " + str(self.quantity)
+        row = "Name: " + self.name + "-" + "Quantity: " + str(self.quantity)
         return row
 
     def delete(self, using=None, keep_parents=False):
